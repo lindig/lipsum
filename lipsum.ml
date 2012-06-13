9dfdd26462238797f3cd04f2e23a89ff9e9c9832
Christian Lindig <lindig@gmail.com>
2012-06-13 16:46:45 +0200

    Converted tabs to spaces. Now using tokens defined in parser.

--- lipsum.ml
+++ lipsum.ml
@@ -1,5 +1,6 @@
 
 module S = Scanner
+module P = Parser
 
 exception Error of string
 let error fmt = Printf.kprintf (fun msg -> raise (Error msg)) fmt
@@ -8,34 +9,34 @@ let eprintf   = Printf.eprintf
 let (@@) f x = f x
 
 let scan file =
-	let f  		= open_in file in
-	let lexbuf 	= Lexing.from_channel f in
-	let rec loop lexbuf =
-		match S.token' lexbuf with
-		| S.EOF  -> print_endline @@ S.to_string S.EOF
-		| tok  -> ( print_endline @@ S.to_string tok
-				  ; loop lexbuf
-				  )
-	in
-		( loop lexbuf
-		; close_in f
-		)
+    let f       = open_in file in
+    let lexbuf  = Lexing.from_channel f in
+    let rec loop lexbuf =
+        match S.token' lexbuf with
+        | P.EOF  -> print_endline @@ S.to_string P.EOF
+        | tok  -> ( print_endline @@ S.to_string tok
+                  ; loop lexbuf
+                  )
+    in
+        ( loop lexbuf
+        ; close_in f
+        )
 
 let main () =
-	let argv    = Array.to_list Sys.argv in
-	let this    = Filename.basename (List.hd argv) in
-	let args    = List.tl argv in
-		match args with
-		| [file] -> scan file
-		| []     -> error "%s: missing filename" this
-		| _::_   -> error "%s: too many file names" this
+    let argv    = Array.to_list Sys.argv in
+    let this    = Filename.basename (List.hd argv) in
+    let args    = List.tl argv in
+        match args with
+        | [file] -> scan file
+        | []     -> error "%s: missing filename" this
+        | _::_   -> error "%s: too many file names" this
 
 
 let () = 
-	try 
-		main (); exit 0
-	with 
-		| Error(msg)         -> eprintf "error: %s\n" msg; exit 1
-		| Failure(msg)       -> eprintf "error: %s\n" msg; exit 1
-		| Scanner.Error(msg) -> eprintf "error: %s\n" msg; exit 1
-		| _ 		         -> Printf.eprintf "some unknown error occurred\n"; exit 1	
+    try 
+        main (); exit 0
+    with 
+        | Error(msg)         -> eprintf "error: %s\n" msg; exit 1
+        | Failure(msg)       -> eprintf "error: %s\n" msg; exit 1
+        | Scanner.Error(msg) -> eprintf "error: %s\n" msg; exit 1
+        | _                  -> Printf.eprintf "some unknown error occurred\n"; exit 1  
--- syntax.ml
+++ syntax.ml
@@ -4,51 +4,51 @@ module SM = Map.Make(String)
 
 
 type code =
-	| Str 		of string
-	| Ref 		of string
+    | Str       of string
+    | Ref       of string
 
 type chunk =
-	| Doc 		of string
-	| Code 		of string * code list
+    | Doc       of string
+    | Code      of string * code list
 
 type t = 
-	{ code:     (code list) SM.t
-	; chunks: 	chunk list
-	}
+    { code:     (code list) SM.t
+    ; chunks:   chunk list
+    }
 
 (* print to stdout *)
 
 let (@@) f x = f x
 
 let empty =
-	{ code   	= SM.empty 
-	; chunks  	= []
-	}
+    { code      = SM.empty 
+    ; chunks    = []
+    }
 
-let extend key v map =
-	if SM.mem key map
-	then SM.add key (v@(SM.find key map)) map
-	else SM.add key v map
+let add key v map =
+    if SM.mem key map
+    then SM.add key (v@(SM.find key map)) map
+    else SM.add key v map
 
 let index chunks =
-	let add t = function
-		| Doc(str)   as d ->  	{ t with chunks = d :: t.chunks }
-		| Code(n,cs) as c ->  	{ code   = extend n cs t.code
-								; chunks = c::t.chunks
-								}
-	in
-		List.fold_left add empty chunks
+    let add t = function
+        | Doc(str)   as d ->    { t with chunks = d :: t.chunks }
+        | Code(n,cs) as c ->    { code   = add n cs t.code
+                                ; chunks = c::t.chunks
+                                }
+    in
+        List.fold_left add empty chunks
 
 
 let code = function
-	| Str(str) 		-> Printf.printf "|%s|" 	str
-	| Ref(str) 		-> Printf.printf "|<<%s>>|" str
-		
+    | Str(str)      -> Printf.printf "|%s|"     str
+    | Ref(str)      -> Printf.printf "|<<%s>>|" str
+        
 let chunk map = function 
-	| Doc(str) 		 -> Printf.printf "|@ %s|" 	str
-	| Code(name,cs)  -> 
-			( Printf.printf "|<<%s>>=" name
-			; List.iter code @@ SM.find name map
-			)
+    | Doc(str)       -> Printf.printf "|@ %s|"  str
+    | Code(name,cs)  -> 
+            ( Printf.printf "|<<%s>>=" name
+            ; List.iter code cs
+            )
 
 let print litprog = List.iter (chunk litprog.code) litprog.chunks
