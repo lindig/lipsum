3f2978e85679e558eb1ceed2282780926e5a6621
Christian Lindig <lindig@gmail.com>
2012-06-23 22:50:09 +0200

    File handling refactored, new cmd line options: chunks, expand.

--- lipsum.ml
+++ lipsum.ml
@@ -5,12 +5,21 @@ module P = Parser
 exception Error of string
 let error fmt = Printf.kprintf (fun msg -> raise (Error msg)) fmt
 let eprintf   = Printf.eprintf
+let printf    = Printf.printf
 
 let (@@) f x = f x
 
-let scan file =
-    let f       = open_in file in
-    let lexbuf  = Lexing.from_channel f in
+let process f = function
+    | Some path -> 
+        let io = open_in path in 
+            ( f io
+            ; close_in io
+            )
+    | None -> f stdin
+            
+
+let scan io =
+    let lexbuf  = Lexing.from_channel io in
     let rec loop lexbuf =
         match S.token' lexbuf with
         | P.EOF  -> print_endline @@ S.to_string P.EOF
@@ -18,22 +27,36 @@ let scan file =
                   ; loop lexbuf
                   )
     in
-        ( loop lexbuf
-        ; close_in f
-        )
+        loop lexbuf
 
-let parse file =
-    let f       = open_in file in
-    let lexbuf  = Lexing.from_channel f in
+let doc io =
+    let lexbuf  = Lexing.from_channel io in
     let ast     = P.litprog S.token' lexbuf in
-    let doc     = Syntax.index ast in
-        ( Syntax.print doc
-        ; close_in f
-        )
+        Syntax.index ast
+        
+
+let parse io =
+    Syntax.print @@ doc io
+
+
+let print_chunk chunk doc =
+    let rec loop = function
+        | Syntax.Str(s) -> print_string s
+        | Syntax.Ref(s) -> List.iter loop (Syntax.SM.find s doc.Syntax.code)
+    in
+        List.iter loop (Syntax.SM.find chunk doc.Syntax.code)
+
+let expand chunk io =
+    print_chunk chunk @@ doc io
+
+let chunks io =
+    let d = doc io in
+    let print key v = print_endline key in
+        Syntax.SM.iter print d.Syntax.code
 
 let help this =
-    ( eprintf "%s scan file.lp\n" this
-    ; eprintf "%s parse file.lp\n" this
+    ( eprintf "%s scan [file.lp]\n" this
+    ; eprintf "%s parse [file.lp]\n" this
     ; exit 1
     )  
       
@@ -42,8 +65,13 @@ let main () =
     let this    = Filename.basename (List.hd argv) in
     let args    = List.tl argv in
         match args with
-        | "scan" ::file::[]     -> scan file
-        | "parse"::file::[]     -> parse file
+        | "scan" ::file::[]     -> process scan  (Some file)
+        | "parse"::file::[]     -> process parse (Some file)
+        | "scan" ::[]           -> process scan   None
+        | "parse"::[]           -> process parse  None
+        | "expand"::s::file::[] -> process (expand s) (Some file)
+        | "chunks"::s::file::[] -> process chunks (Some file)
+        
         | "help"::_             -> help this
         | "-help"::_            -> help this
         | []                    -> help this
@@ -57,4 +85,6 @@ let () =
         | Error(msg)         -> eprintf "error: %s\n" msg; exit 1
         | Failure(msg)       -> eprintf "error: %s\n" msg; exit 1
         | Scanner.Error(msg) -> eprintf "error: %s\n" msg; exit 1
+        (*
         | _                  -> Printf.eprintf "some unknown error occurred\n"; exit 1  
+        *)
